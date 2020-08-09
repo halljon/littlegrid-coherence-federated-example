6f97d01b32f617b9d2b9b523bc1a256671ccd265
halljon <jon>
2020-08-09 15:17:51 +0100

    Tidying code.

--- src/test/java/io/halljon/coherence/testsupport/cluster/ClusterTestSupportUtils.java
+++ src/test/java/io/halljon/coherence/testsupport/cluster/ClusterTestSupportUtils.java
@@ -1,6 +1,7 @@
 package io.halljon.coherence.testsupport.cluster;
 
 import com.tangosol.net.CacheFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.littlegrid.ClusterMemberGroup;
 import org.littlegrid.ClusterMemberGroupUtils;
 import org.littlegrid.impl.SimpleKeepAliveClusterMemberGroup;
@@ -11,9 +12,10 @@ import java.lang.management.ManagementFactory;
  * Test support class to make tests free from configuration/setup type of code.
  */
 public final class ClusterTestSupportUtils {
+    private static final int MAXIMUM_CLUSTER_NAME_LENGTH = 66;
     private static final String PROCESS_NAME = ManagementFactory.getRuntimeMXBean().getName();
-    private static final String LDN_CLUSTER_NAME = "LDN-" + PROCESS_NAME;
-    private static final String NYC_CLUSTER_NAME = "NYC-" + PROCESS_NAME;
+    private static final String LDN_CLUSTER_NAME = StringUtils.truncate("LDN-" + PROCESS_NAME, MAXIMUM_CLUSTER_NAME_LENGTH);
+    private static final String NYC_CLUSTER_NAME = StringUtils.truncate("NYC-" + PROCESS_NAME, MAXIMUM_CLUSTER_NAME_LENGTH);
 
     private ClusterTestSupportUtils() {
         // Private constructor to prevent creation
@@ -28,19 +30,11 @@ public final class ClusterTestSupportUtils {
     }
 
     public static ClusterMemberGroup getOrCreateLdnCluster() {
-        return ClusterMemberGroupUtils.newBuilder()
-                .setClusterName(getLdnClusterName())
-                .setStorageEnabledCount(1)
-                .setClusterMemberGroupInstanceClassName(SimpleKeepAliveClusterMemberGroup.class.getName())
-                .buildAndConfigureForStorageDisabledClient();
+        return getClusterMemberGroup(getLdnClusterName());
     }
 
     public static ClusterMemberGroup getOrCreateNycCluster() {
-        return ClusterMemberGroupUtils.newBuilder()
-                .setClusterName(getNycClusterName())
-                .setStorageEnabledCount(1)
-                .setClusterMemberGroupInstanceClassName(SimpleKeepAliveClusterMemberGroup.class.getName())
-                .buildAndConfigureForStorageDisabledClient();
+        return getClusterMemberGroup(getNycClusterName());
     }
 
     public static void joinLdnClusterAsStorageDisabledClient() {
@@ -51,5 +45,13 @@ public final class ClusterTestSupportUtils {
     public static void joinNycClusterAsStorageDisabledClient() {
         CacheFactory.shutdown();
         getOrCreateNycCluster();
+    }
+
+    private static ClusterMemberGroup getClusterMemberGroup(String clusterName) {
+        return ClusterMemberGroupUtils.newBuilder()
+                .setClusterName(clusterName)
+                .setStorageEnabledCount(1)
+                .setClusterMemberGroupInstanceClassName(SimpleKeepAliveClusterMemberGroup.class.getName())
+                .buildAndConfigureForStorageDisabledClient();
     }
 }
